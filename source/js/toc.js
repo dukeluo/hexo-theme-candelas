d0ebe9954bf66821870657accb692b7c38e9ebad
duke <dukeluo@outlook.com>
2020-04-10 23:28:41 +0800

    feat: modify toc style in the post page

--- source/js/toc.js
+++ source/js/toc.js
@@ -1,4 +1,4 @@
-;(function ($) {
+; (function ($) {
   function getHeadlineOffset() {
     return $("#main h3, #main h4").map(function () {
       return $(this).offset().top;
@@ -6,22 +6,29 @@
   }
 
   function getTocLink() {
-    return $("#toc-wrap .toc-link");
+    return $(".toc-wrap .toc-link");
   }
 
-  $(window).scroll(function() {
-    var headlineInterval = Array.prototype.slice.call(getHeadlineOffset());
-    headlineInterval.push(Math.max($(document).height(), $(window).height()));
-    headlineInterval = headlineInterval.map(function(i) { return Math.floor(i)});
-    var $tocLinks = getTocLink();
-    var scrollY = $(window).scrollTop();
+  function getHeaderHeight() {
+    return $('#header').height();
+  }
+
+  var $tocLinks = getTocLink();
+  var headerHeight = getHeaderHeight();
+  var headlineInterval = Array.prototype.slice.call(getHeadlineOffset());
+
+  headlineInterval.push(Math.max($(document).height(), $(window).height()));
+  headlineInterval = headlineInterval.map(function (i) { return Math.floor(i) });
+
+  $(window).scroll(function () {
+    var scrollY = $(window).scrollTop() + headerHeight;
 
     for (let i = 0; i < headlineInterval.length - 1; i++) {
       var offsetA = headlineInterval[i];
-      var offsetB = headlineInterval[i+1];
+      var offsetB = headlineInterval[i + 1];
 
       if (scrollY >= offsetA && scrollY < offsetB) {
-        $tocLinks.each(function(index) {
+        $tocLinks.each(function (index) {
           if (index === i) {
             $(this).addClass("toc-link-active");
           } else {
