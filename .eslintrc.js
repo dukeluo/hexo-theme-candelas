8feecd131eabbf5c92f610b2e91f1495e542512e
duke <dukeluo@outlook.com>
2020-05-03 13:58:40 +0800

    #N/A chore: add lint for project

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'env': {
+    'browser': true,
+  },
+  'extends': 'eslint:recommended',
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error",
+      "always"
+    ],
+    "no-console": 0
+  },
+  'plugins': [
+    "html",
+  ],
+  "settings": {
+    "html/html-extensions": [".ejs"],
+  }
+};
