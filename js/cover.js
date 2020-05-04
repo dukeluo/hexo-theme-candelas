94ce2e263f73b746c631bec2fd5d8759f35c0488
DukeLuo <dukeluo@outlook.com>
2020-05-04 08:53:52 +0000

    Deploying to gh-pages from  317c3f1cadcdefe27146c6121abb5bcc96dc57d8

--- js/cover.js
+++ js/cover.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-extra-semi
 ;(function ($) {
   var $banner = $('#cover #banner');
-  var bannerUrls = $banner.attr("data-banners").split(';');
+  var bannerUrls = $banner.attr('data-banners').split(';');
   var randomBannerUrl = bannerUrls[Math.floor(Math.random() * bannerUrls.length)];
 
   $banner.css('background-image', 'url(' + randomBannerUrl + ')');
+// eslint-disable-next-line no-undef
 })(jQuery);
