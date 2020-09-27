20cc2399c544040d685f6f82a2ed74cf2c474269
DukeLuo <dukeluo@outlook.com>
2020-09-27 16:00:46 +0000

    Deploying to gh-pages from  028fef7d5152d90a803b9f86b3fccd20317ae6af

---
+++ js/fingerprint.js
@@ -0,0 +1,41 @@
+// eslint-disable-next-line no-extra-semi
+;(function (window) {
+  const options = {
+    excludes: {
+      plugins: true,
+      localStorage: true,
+      adBlock: true,
+      screenResolution: true,
+      availableScreenResolution: true,
+      enumerateDevices: true,
+      pixelRatio: true,
+      doNotTrack: true
+    }
+  };
+
+  function getFingerprint() {
+    return new Promise((resolve, reject) => {
+      async function getHash() {
+        try {
+          const components = await window.Fingerprint2.getPromise(options);
+          const values = components.map(component => component.value);
+
+          return window.Fingerprint2.x64hash128(values.join(''), 31);
+        } catch (e) {
+          reject(e);
+        }
+      }
+
+      if (window.requestIdleCallback) {
+        requestIdleCallback(async () => resolve(await getHash()));
+      } else {
+        setTimeout(async () => resolve(await getHash()), 500);
+      }
+    });
+  }
+
+  const candelas = window.candelas || {};
+
+  candelas.getFingerprint = getFingerprint;
+  window.candelas = candelas;
+})(window);
