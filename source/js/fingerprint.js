83ef6defad1013689c6e6f3c9b670d8dcf164f55
duke <dukeluo@outlook.com>
2020-09-27 23:57:38 +0800

    #16 feat: add method to get fingerprint and do some refactor

---
+++ source/js/fingerprint.js
@@ -0,0 +1,40 @@
+;(function (window) {
+  const options = {
+    excludes: {
+      plugins: true,
+      localStorage: true,
+      adBlock: true,
+      screenResolution: true,
+      availableScreenResolution: true,
+      enumerateDevices: true,
+      pixelRatio: true,
+      doNotTrack: true
+    }
+  };
+
+  function getFingerprint() {
+    return new Promise((resolve, reject) => {
+      async function getHash() {
+        try {
+          const components = await Fingerprint2.getPromise(options);
+          const values = components.map(component => component.value);
+
+          return Fingerprint2.x64hash128(values.join(''), 31);
+        } catch (e) {
+          reject(e);
+        }
+      }
+
+      if (window.requestIdleCallback) {
+        requestIdleCallback(async () => resolve(await getHash()));
+      } else {
+        setTimeout(async () => resolve(await getHash()), 500);
+      }
+    });
+  }
+
+  const candelas = window.candelas || {};
+
+  candelas.getFingerprint = getFingerprint;
+  window.candelas = candelas;
+})(window);
---
+++ source/js/post-like-gist.js
@@ -0,0 +1,85 @@
+;(function (window) {
+  const FILE_NAME = 'post-like.json';
+
+  async function updatePostLikeGist(octokit, postLikeGistId, content) {
+    const { status, data } = await octokit.request('PATCH /gists/{gistId}', {
+      gistId: postLikeGistId,
+      description: 'description',
+      files: {
+        [FILE_NAME]: {
+          content: JSON.stringify(content)
+        }
+      }
+    });
+
+    if (status !== 200) {
+      return;
+    }
+
+    return {
+      id: data.id,
+      files: data.files
+    };
+  }
+
+  async function getPostLikeGist(octokit) {
+    const fileGistIdMap = await getFileGistIdMap(octokit);
+    const postLikeGistId = fileGistIdMap.get(Array.from(fileGistIdMap.keys()).find(file => file.filename === FILE_NAME));
+    const { status, data } = await octokit.request('GET /gists/{gistId}', {
+      gistId: postLikeGistId
+    });
+
+    if (status !== 200) {
+      return;
+    }
+
+    return {
+      id: data.id,
+      files: data.files
+    } || await createPostLikeGist(octokit);
+  }
+
+  async function getFileGistIdMap(octokit) {
+    const { status, data } = await octokit.request('GET /gists');
+
+    if (status !== 200) {
+      return;
+    }
+
+    const fileGistIdMap = new Map();
+    data.reduce(function (map, gist) {
+      Object.values(gist.files).forEach(file => map.set(file, gist.id));
+      return map;
+    }, fileGistIdMap);
+
+    return fileGistIdMap;
+  }
+
+  async function createPostLikeGist(octokit) {
+    const { status, data } = await octokit.request('POST /gists', {
+      accept: 'application/vnd.github.v3+json',
+      description: 'A gist to record likes of each post. It\'s used by Hexo theme candelas. https://github.com/DukeLuo/hexo-theme-candelas',
+      files: {
+        [FILE_NAME]: {
+          content: JSON.stringify([])
+        }
+      },
+      public: true
+    });
+
+    if (status !== 201) {
+      return;
+    }
+
+    return {
+      id: data.id,
+      files: data.files
+    };
+  }
+
+  const candelas = window.candelas || {};
+
+  candelas.getPostLikeGist = getPostLikeGist;
+  candelas.updatePostLikeGist = updatePostLikeGist;
+  window.candelas = candelas;
+})(window);
