2e1454d7df2049dd94df618f803532d2e1c3a3c2
DukeLuo <dukeluo@outlook.com>
2020-11-28 13:31:37 +0000

    Deploying to gh-pages from  a10e4d0264f5e73fe56c115c5eafbe6279dcf508

--- js/chocolate.js
+++ js/chocolate.js
@@ -1,5 +1,6 @@
-let dogAsciiText = `
-            ,****.        .
+(function () {
+  let dogAsciiText =
+`           ,****.        .
             /,,,,//                           .
         . /**//,.,/.                  ,//*,,/.
           /**(%(,,,(,((((((,  .   .,//*,,//*
@@ -23,12 +24,12 @@ let dogAsciiText = `
   #((((((((#((((//////////(/%%#((((((((((%,
 ./#((((((((((#%(((((((((%%((((((((#%%%%%*
 `;
-let textStyle = 'color:#faae2b';
-let linkStyle = `
-background-image: linear-gradient(90deg, #b7efcd 0%, #4cd3c2 100%);
-padding: 5px 1px;
-`;
+  let textStyle = 'color:#faae2b';
+  let linkStyle = `
+    background-image: linear-gradient(90deg, #b7efcd 0%, #4cd3c2 100%);
+    padding: 5px 1px;`;
 
-console.log(dogAsciiText);
-console.log('%c 网络一线牵，珍惜这段缘!', textStyle);
-console.log('%c Candelas https://github.com/DukeLuo/hexo-theme-candelas', linkStyle);
+  console.log(dogAsciiText);
+  console.log('%c 网络一线牵，珍惜这段缘!', textStyle);
+  console.log('%c Candelas https://github.com/DukeLuo/hexo-theme-candelas', linkStyle);
+})();
