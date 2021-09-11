d298e36f26934b38d36ae9818c18fdf8b2af2d47
DukeLuo <dukeluo@outlook.com>
2021-09-11 06:36:53 +0000

    Deploying to gh-pages from  67ada3ad4f755f89da931ebbdc4106edd259b255

--- js/script.js
+++ js/script.js
@@ -1,36 +1,4 @@
 (function($){
-  // Search
-  let $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
-
-  let startSearchAnim = function(){
-    isSearchAnim = true;
-  };
-
-  let stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
-  });
-
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
-
   // Caption
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
